afcd01971461f60b3f941f30c47e1963b8120929
Yasuo Harada <slywalker.net@gmail.com>
2013-07-04 11:17:32 +0900

    fixed how to get class, and add test case

--- View/Helper/BoostCakeHtmlHelper.php
+++ View/Helper/BoostCakeHtmlHelper.php
@@ -14,12 +14,18 @@ class BoostCakeHtmlHelper extends HtmlHelper {
  */
 	public function useTag($tag) {
 		$args = func_get_args();
+
+		if ($tag === 'radio') {
+			$class = (isset($args[3]['class'])) ? $args[3]['class'] : 'radio';
+			unset($args[3]['class']);
+		}
+
 		$html = call_user_func_array(array('parent', 'useTag'), $args);
 
 		if ($tag === 'radio') {
 			$regex = '/(<label)(.*?>)/';
 			if (preg_match($regex, $html, $match)) {
-				$html = $match[1] . ' class="radio'.(isset($args[3]['class']) ? ' '.$args[3]['class'] : '').'"' . $match[2] . preg_replace($regex, ' ', $html);
+				$html = $match[1] . ' class="' . $class . '"' . $match[2] . preg_replace($regex, ' ', $html);
 			}
 		}
 
